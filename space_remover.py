fc52d19b7b692c9f1cb1218c6b1c88be4f88d7da
roman-kojnok <roman.kojnok@protonmail.com>
2022-09-04 13:10:37 +0100

    Innitial commit

--- space_remover.py
+++ space_remover.py
@@ -5,5 +5,5 @@ path = os.getcwd()
 filenames = os.listdir(path)
 
 for filename in filenames:
-    # os.rename(filename, filename.replace(" ", "-").lower())
-    os.rename(filename, filename.replace(" . ", "-"))
+    # os.rename(filename, filename.replace(" ", "_").lower())
+    os.rename(filename, filename.replace(" ", "_"))
